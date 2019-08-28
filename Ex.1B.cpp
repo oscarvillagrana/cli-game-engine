949a6acd501c6744ed082d90bf6d0c77f9d4c9fb
oscarevolves <>
2019-08-27 17:23:34 -0700

    game scores

---
+++ Ex.1B.cpp
@@ -0,0 +1,15 @@
+// Exercise 1B: Oscar Villagrana
+// This program prints out as many asterisks as the user wants.
+
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int home_score, visitor_score;
+
+    cout >> 
+
+}
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 projects using fundamentals
 
 For the course CS 110B at City College of San Francisco. Instructed by Max Luttrell in Fall 2019.
+
+### Data Types, Input/Output, If, If-Else

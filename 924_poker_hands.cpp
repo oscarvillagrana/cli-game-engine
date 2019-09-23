ff4fbe05f8960124dddc1f9c6d26d5e4844c478e
oscarevolves <>
2019-09-23 01:07:54 -0700

    version 1

--- 924_poker_hands.cpp
+++ 924_poker_hands.cpp
@@ -13,42 +13,20 @@ bool containsStraight(int hand[]);
 bool containsFullHouse(int hand[]);
 bool containsFourOfaKind(int hand[]);
 
+
 // Here is code that tests a containsPair function
 
 int main() {
-        int hand[] = {2, 5, 3, 2, 9};
 
-int card_1 = 0
-int card_2 = 0
-int card_3 = 0
-int card_4 = 0
-int card_5 = 0
+    int hand[] = {2, 5, 3, 2, 9};
 
+    if (containsPair(hand)) {
+            cout << "Pair!" << endl;
+    }
 
+    return 0;
 
-
-        if (containsPair(hand)) {
-                cout << "Pair!" << endl;
-        }
-
-        return 0;
 }
-
-cout << "Enter five numeric cards, no face cards. Use 2 - 9: " << endl;
-cout << "Card 1: " << endl;
-cin >> card_1 >> endl;
-
-cout << "Card 2: " << endl;
-cin >> card_2 >> endl;
-
-cout << "Card 3: " << endl;
-cin >> card_3 >> endl;
-
-cout << "Card 4: " << endl;
-cin >> card_4 >> endl;
-
-cout << "Card 5: " << endl;
-cin >> card_5 >> endl;
 
 
 // Your program should be able to recognize hand categories
@@ -58,117 +36,237 @@ cin >> card_5 >> endl;
 // Each function must accept an int array as a parameter.  
 // You can assume that the array will have five elements.  
 
-bool searchSingleInt()
-{
-    int size = 5;
-    int list[size] = {3, 3, 3, 4, 5};
-    int value = 3;
+int print_hand_type() {
 
-    int pos = 0;
-    bool isFound = false;
+    if (containsPair(hand)) 
+        cout << "Pair!" << endl;
+    
 
-    while (!isFound && pos < size)
-    {
-        if (list[pos] == value)
-            cout << "1" << endl;
-            // isFound == true;
-            // if (isFound)
-        pos++;
-    }
-    return isFound;
+    if (containsThreeOfaKind(hand)) 
+        cout << "Three Of A Kind" << endl;
+    
 
+    if (containsTwoPair(hand)) 
+        cout << "Two Pair!" << endl;
+    
+
+    if (containsFourOfaKind(hand)) 
+        cout << "Four Of a Kind!" << endl;
+    
+
+    if (containsStraight(hand)) 
+        cout << "Straight!" << endl;
+    
+
+    if (containsFullHouse(hand)) 
+        cout << "Full House!" << endl;
 }
+
+
+int collect_values() {
+
+    int NUM_CARDS = 5;
+    int hand[NUM_CARDS];
+
+    int card_1 = 0;
+    int card_2 = 0;
+    int card_3 = 0;
+    int card_4 = 0;
+    int card_5 = 0;
+
+    hand[0] = card_1;
+    hand[1] = card_2;
+    hand[2] = card_3;
+    hand[3] = card_4;
+    hand[4] = card_5;
+
+    cout << "Enter five numeric cards, no face cards. Use 2 - 9: " << endl;
+    cout << "Card 1: " << endl;
+    cin >> card_1 >> endl;
+
+    cout << "Card 2: " << endl;
+    cin >> card_2 >> endl;
+
+    cout << "Card 3: " << endl;
+    cin >> card_3 >> endl;
+
+    cout << "Card 4: " << endl;
+    cin >> card_4 >> endl;
+
+    cout << "Card 5: " << endl;
+    cin >> card_5 >> endl;
+}
+
+
+// High Card: No matching cards and not a straight
+// 2, 5, 3, 8, 7
+
+// High Card:
+// If not one of the better hand types, then it is a High Card hands
+
+
+int highCard(int hand[]) {
+
+    int NUM_CARDS = 5;
+    int highCard = hand[0];
+    for (int i=0; i< NUM_CARDS; i++) {
+        if (hand[i] > highCard);
+            highCard = hand[i];
+    }
+    return highCard;
+}
+
 
 
 // Pair: Two of the cards are identical
 // 2, 5, 3, 5, 7
-bool containsPair(int hand[])
-{
-    int list[];
-    int size = 5;
-    int value;
-
-    int pos = 0;
-    bool isFound = false;
-
-    while (!isFound && pos < size)
-    {
-        if (list[pos] == value)
-            isFound == true;
-        pos++;
+bool containsPair(int hand[]) {
+    
+    for (int handIndex = 0; handIndex < NUM_CARDS; handIndex++) {
+        
+        for (int comparisonIndex = handIndex +1; comparisonIndex < NUM_CARDS; comparisonIndex++) {
+            // find matching pair
+    
+            if ( hand[handIndex] == hand[comparisonIndex])
+                return true;
+        }
     }
-    return isFound;
+    return false;
 }
 
-
-// Two Pair: Two different pairs
-// 2, 5, 3, 5, 3
-bool  containsTwoPair(int hand[])
 
 // Three of a kind: Three matching cards
 // 5, 5, 3, 5, 7
-bool  containsThreeOfaKind(int hand[])
+bool  containsThreeOfaKind(int hand[]) {
 
-// Straight: Card values can be arranged in order, regardless of the order
-bool  containsStraight(int hand[])
-{
-    const int arraySize = 5;
-    int hand[arraySize] = {3, 4, 5, 6, 7};
-    int toTheLeft[arraySize];
-    int toTheRight[arraySize];
+    for (int handIndex = 0; handIndex < NUM_CARDS; handIndex++) {
+        int matchingCards = 0;
 
-    for (int i = 0; i < arraySize; i++)
-    {
-        toTheLeft[i] = hand[i] - 1;
-        toTheRight[i] = hand[i] + 1;
-
-        cout << toTheRight[i] << endl;
-        cout << toTheLeft[i] << endl;
-
-        bool isStraight = true;
-        int pos = 0;
-
-        while (!isStraight && pos < arraySize)
-        {
-        if (hand[pos] == toTheLeft[pos])
-            isStraight == true;
-        else if (hand[pos] == toTheRight[pos])
-            isStraight == true;
-        pos++;
+        for (int comparisonIndex = handIndex +1; comparisonIndex < NUM_CARDS; comparisonIndex++) {
+            if ( hand[cardsIndex] == hand[comparisonIndex])
+                matchingCards++;
+         
+            if (matchingCards == 2)
+                return true;
         }
+    }
+    return false;
+}
 
-        return isStraight;
-        if (isStraight)
-            cout << "Its straight!";
+// Two Pair: Two different pairs
+// 2, 5, 3, 5, 3
+bool  containsTwoPair(int hand[]) {
+    int counterVariable = 0;
+
+    for (int handIndex = 0; handIndex < NUM_CARDS; handIndex++) {
+        
+        for (int comparisonIndex = handIndex +1; comparisonIndex < NUM_CARDS; comparisonIndex++) {
+            // find matching pair
+            
+            if ( hand[cardsIndex] == hand[comparisonIndex])
+                counterVariable++;
+        }
     }
 
+    if (counterVariable == 2)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
+
+
+
+// Straight: Card values can be arranged in order, regardless of the order
+bool  containsStraight(int hand[]) {
+    
+    int lowCard = hand[0];
+    
+    for (int i = 0; i < NUM_CARDS; i++) {
+        if (hand[i] < lowCard) {
+            lowCard = hand[i];
+        }
+    }
+    for (int i = 1; i<=4; i++) {
+        bool hasStraight = false;
+        
+        for (int cardsIndex = 0; cardsIndex < NUM_CARDS; cardsIndex++){
+            if (hand[cardsIndex] == (low + 1)) {
+                hasStraight = true;
+            }
+        }
+        if (hasStraight == false) {
+            return false;
+    }
+    return true;
+}
+
+
 
 
 
 // Full House: A pair, and a three of a kind
 // 5, 7, 5, 7, 7
-bool  containsFullHouse(int hand[])
+bool  containsFullHouse(int hand[]) {
+    
+    int ThreeOfAKind = 0;
+
+    for (int handIndex = 0; handIndex < NUM_CARDS; handIndex++) {
+        int matchingCards = 0;
+
+        for (int comparisonIndex = handIndex +1; comparisonIndex < NUM_CARDS; comparisonIndex++) {
+            
+            if ( hand[cardsIndex] == hand[comparisonIndex])
+                ThreeOfAKind = hand[cardsIndex];
+         
+            if (ThreeOfAKind == 0)
+                return false;
+        }
+    }
+
+    int hasPair = 0;
+    for (int handIndex = 0; handIndex < NUM_CARDS; handIndex++)
+    {
+        for (int comparisonIndex = handIndex +1; comparisonIndex < NUM_CARDS; comparisonIndex++)
+        {
+            // find matching pair
+            if ( hand[handIndex] == hand[comparisonIndex])
+
+            if (hand[handIndex] != ThreeOfAKind)
+                return true;
+        }
+    }
+
+}
+
+
+
 
 // Four of a kind: Four matching cards
 // 2, 5, 5, 5, 5
 bool  containsFourOfaKind(int hand[])
+{
 
-// High Card: No matching cards and not a straight
-// 2, 5, 3, 8, 7
+    for (int handIndex = 0; handIndex < NUM_CARDS; handIndex++)
+    {
+        int matchingCards = 0;
 
-High Card:
-If not one of the better hand types, then it is a High Card hands
-
-
-// Here is code that tests a containsPair function
-
-int main() {
-        int hand[] = {2, 5, 3, 2, 9};
-
-        if (containsPair(hand)) {
-                cout << "Pair!" << endl;
+        for (int comparisonIndex = handIndex +1; comparisonIndex < NUM_CARDS; comparisonIndex++)
+        {
+            if ( hand[cardsIndex] == hand[comparisonIndex])
+                matchingCards++;
+         
+            if (matchingCards == 3)
+                return true;
         }
+    }
+    return false;
 
-        return 0;
 }
+
+
+
+
--- 924_test.cpp
+++ 924_test.cpp
@@ -2,22 +2,200 @@
 
 using namespace std;
 
-int main()
-{ 
-    int size = 5;
-    int list[size] = {3, 3, 3, 4, 5};
-    int value = 3;
+int print_hand_type(int hand[]);
+bool containsFullHouse(int hand[]);
+bool containsStraight(int hand[]);
+bool containsFourOfaKind(int hand[]);
+bool containsThreeOfaKind(int hand[]);
+bool containsTwoPair(int hand[]);
+bool containsPair(int hand[]);
 
-    int pos = 0;
-    bool isFound = false;
+int main() 
+{
 
-    while (!isFound && pos < size)
-    {
-        if (list[pos] == value)
-            cout << "1" << endl;
-            // isFound == true;
-            // if (isFound)
-        pos++;
-    }
-    return isFound;
+// Full House: A pair, and a three of a kind
+// int hand[] = {2, 2, 2, 3, 3};
+
+// Straight: Card values can be arranged in order, regardless of the order
+// int hand[] = {2, 3, 4, 5, 6};
+
+// Four of a kind: Four matching cards
+// int hand[] = {2, 5, 5, 5, 5};
+
+// Three of a kind: Three matching cards
+// int hand[] = {5, 5, 3, 5, 7};
+
+// Two Pair: Two different pairs
+// int hand[] = {2, 5, 3, 5, 3};
+
+// Pair
+// int hand[] = {2, 5, 3, 2, 9};
+    print_hand_type(hand);
+
+    return 0;
 }
+
+int print_hand_type(int hand[]) {
+
+    if (containsFullHouse(hand)) {
+        cout << "Full House!" << endl;
+        return 0;
+    }
+    if (containsStraight(hand)) {
+        cout << "Straight!" << endl;
+        return 0;
+    }
+    if (containsFourOfaKind(hand)) {
+        cout << "Four Of a Kind!" << endl;
+        return 0;
+    }
+    if (containsTwoPair(hand)) {
+        cout << "Two Pair!" << endl;
+        return 0;
+    }
+    if (containsThreeOfaKind(hand)) {
+        cout << "Three Of A Kind" << endl;
+        return 0;
+    }
+    if (containsPair(hand)) {
+        cout << "Pair!" << endl;
+        return 0;
+    }
+}
+
+bool  containsFullHouse(int hand[]) {
+
+    int NUM_CARDS = 5;
+    int ThreeOfAKind = 0;
+
+    for (int handIndex = 0; handIndex < NUM_CARDS; handIndex++) {
+        int matchingCards = 0;
+
+        for (int comparisonIndex = handIndex +1; comparisonIndex < NUM_CARDS; comparisonIndex++) {
+            
+            if ( hand[handIndex] == hand[comparisonIndex]){
+                ThreeOfAKind = hand[handIndex];
+            }
+         
+            if (ThreeOfAKind == 0) {
+                return false;
+            }
+        }
+    }
+}
+
+
+bool containsStraight(int hand[]) {
+    
+    int NUM_CARDS = 5;
+    int lowCard = hand[0];
+    
+    for (int i = 0; i < NUM_CARDS; i++) {
+
+        if (hand[i] < lowCard) {
+            lowCard = hand[i];
+        }
+    }
+    
+    for (int i = 1; i<=4; i++) {
+        
+        bool hasStraight = false;
+        for (int handIndex = 0; handIndex < NUM_CARDS; handIndex++){
+
+            if (hand[handIndex] == (lowCard + 1)) {
+                hasStraight = true;
+            }
+        }
+    
+        if (hasStraight == false) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+
+bool containsFourOfaKind(int hand[])
+{
+    int NUM_CARDS = 5;
+
+    for (int handIndex = 0; handIndex < NUM_CARDS; handIndex++)
+    {
+        int matchingCards = 0;
+
+        for (int comparisonIndex = handIndex +1; comparisonIndex < NUM_CARDS; comparisonIndex++)
+        {
+            if ( hand[handIndex] == hand[comparisonIndex])
+                matchingCards++;
+         
+            if (matchingCards == 3)
+                return true;
+        }
+    }
+    return false;
+
+}
+
+bool containsThreeOfaKind(int hand[]) {
+
+    int NUM_CARDS = 5;
+
+    for (int handIndex = 0; handIndex < NUM_CARDS; handIndex++) {
+        int matchingCards = 0;
+
+        for (int comparisonIndex = handIndex +1; comparisonIndex < NUM_CARDS; comparisonIndex++) {
+            if ( hand[handIndex] == hand[comparisonIndex])
+                matchingCards++;
+         
+            if (matchingCards == 2)
+                return true;
+        }
+    }
+    return false;
+}
+
+bool  containsTwoPair(int hand[]) {
+
+    int NUM_CARDS = 5;
+
+    int counterVariable = 0;
+
+    for (int handIndex = 0; handIndex < NUM_CARDS; handIndex++) {
+        
+        for (int comparisonIndex = handIndex +1; comparisonIndex < NUM_CARDS; comparisonIndex++) {
+            // find matching pair
+            
+            if ( hand[handIndex] == hand[comparisonIndex])
+                counterVariable++;
+        }
+    }
+
+    if (counterVariable == 2)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+
+bool containsPair(int hand[]) {
+
+    int NUM_CARDS = 5;
+    
+    for (int handIndex = 0; handIndex < NUM_CARDS; handIndex++) {
+        
+        for (int comparisonIndex = handIndex +1; comparisonIndex < NUM_CARDS; comparisonIndex++) {
+    
+            // find matching pair
+            if ( hand[handIndex] == hand[comparisonIndex])
+                return true;
+        }
+    }
+    return false;
+}
+

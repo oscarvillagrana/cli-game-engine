4bddf9eb1df9b8a6d8bfb8ae00c815f83d5c1b4c
Oscar Villagrana <Oscarvillagrana@github.com>
2021-04-29 23:29:05 -0700

    Added Game Engine member functions

--- Asterisks/ast.v4.cpp
+++ Asterisks/ast.v4.cpp
@@ -51,7 +51,7 @@ namespace ge
   class Game
   {
   private:
-    int8_t _id = 0;
+    uint8_t _id = 0;
     string _name = "";
 
     // Game( Game & );                           // no copy constructor
@@ -59,34 +59,43 @@ namespace ge
     Game(){};                                 // no default constructor
 
   public: // Constructors
-    Game( const int8_t & id, const string & name );
+    Game( const uint8_t & id, const string & name );
   
   public: // Member functions
-    const int8_t & id() const;
+    const uint8_t & id() const;
     const string & name() const;
   };
 
   class GameEngine
   {
+    enum goAgain { NO = 0, YES = 1 };
     char _goAgain = 'n';
-    std::vector<ge::Game> vec;
-    // vec.reserve(2);
+    goAgain _goAgain2 = NO;
 
     string _gameToPlay;
-    
-    multimap<int,string> games = { {1,"asterisks"},
-                                   {2,"poker"} };
+    string _gameSelected = "NONE";
 
+    // vec.reserve(2);s
+
+    // multimap<int,string> g = { {1,"asterisks"},
+    //                                {2,"poker"} };
+    // cout << g.second << '(' << g.first << ')' << endl;
     GameEngine(){};                           // no default constructor
   
   public: // member functions
+    std::vector<ge::Game> _games;
     //TODO:
     GameEngine(std::vector<ge::Game> vec);
     //TODO:
-    ge::runcode Start(Game x);
+    ge::runcode start(string game);
     // //TODO: 
-    char setGoAgain(char x);
+    char setGoAgain(char c);
     char getGoAgain();
+    goAgain setGoAgain2(char c);
+    goAgain getGoAgain2();
+    string setGameSelected(string s);
+    string getGameSelected();
+    // std::vector<<ge::Game> getGames();
   };
 }
 
@@ -119,35 +128,53 @@ namespace ge
   // BASE CLASS - Starts the game with defaults                     
   //-------------------------------------------------------------------------------
 
-  Game::Game( const int8_t & id, const string & name )
+  Game::Game( const uint8_t & id, const string & name )
   : _id(id), _name(name) {};
-  const int8_t & Game::id() const { return _id; }
+  const uint8_t & Game::id() const { return _id; }
   const string & Game::name() const { return _name; }
 
   // GameEngine::GameEngine(){};
 
   //TODO:
-  GameEngine::GameEngine(std::vector<ge::Game> vec) 
+  GameEngine::GameEngine(std::vector<ge::Game> vec) : _games(vec)
   {
     cout << "Constructed" << endl;
   };
     //TODO:
-  ge::runcode GameEngine::Start(Game x) {
+  ge::runcode GameEngine::start(string game) 
+  {
     cout << "Started" << endl;
     //TODO:
     cout << "Welcome to the games" << endl;
     cout << "Which game would you like to play? " << endl;        
-    for (auto elem : games) {           
-       cout << elem.second << '(' << elem.first << ')' << endl;
+    for (auto g : _games ) {
+
+      cout << g.name() << '(' << int(g.id()) << ')' << endl;
     }
 
-    cin >> _gameToPlay;
-    cout << _gameToPlay;
+    cin >> _gameSelected;
+    cout << _gameSelected << endl;
     // return OK;
   };
 
-  char GameEngine::setGoAgain(char x) {_goAgain = x; };
+  // std::vector<<ge::Game> getGames() { return _games };
+  char GameEngine::setGoAgain(char c) { _goAgain = c; };
   char GameEngine::getGoAgain() { return _goAgain; };
+
+  string GameEngine::setGameSelected(string s) { _gameSelected = s; };
+  string GameEngine::getGameSelected() { return _gameSelected; };
+
+  GameEngine::goAgain GameEngine::setGoAgain2(char c) 
+  { 
+    if( c == 'y') { _goAgain2 = YES; } 
+    else {   _goAgain2 = NO; }
+  };
+  
+  GameEngine::goAgain GameEngine::getGoAgain2() 
+  { 
+  return _goAgain2; 
+  };
+
 
 
 }
@@ -245,8 +272,8 @@ public:
 
 void test();
 
-// bool testing = false;
-bool testing = true;
+bool testing = false;
+// bool testing = true;
 
 int main()
 {
@@ -256,16 +283,27 @@ int main()
 
   std::vector<ge::Game> vec;
   vec.reserve(2);
-  vec.emplace_back(int(0), string("asterisks"));
+  vec.emplace_back(uint8_t(0), string("asterisks"));
   vec.emplace_back(1, "poker");
 
-  ge::GameEngine game(vec);
+  // TODO: convert int to string
+  // cout << 0 << endl;
+  
+  ge::GameEngine games(vec);
 
-  if (game.Start(asterisks_game)) {
+  if (games.start(asterisks_game.name())) {
+
+      for( ge::Game i : vec ){
+        cout << int(i.id()) << endl;
+      }
+
     // game.SetValues();
     // game.OnValueUpdate(demo.algo);     
-    // game.Start();
+    // game.start();
   }
+
+
+
   return 0;
 }
 
@@ -284,28 +322,60 @@ int main()
 void test()
 {
   
-  // test Game constructors
-  ge::Game f(int8_t(4), string("yep"));
-  ge::Game g(3,"word");
+  // integral sizes
+  //---------------
 
-  // print results
-  cout << f.name() << endl;
-  cout << g.name() << endl;
+  // uint8_t umax = 255;    cout << int(umax) << endl;
+  // int8_t imax = 127;     cout << int(imax) << endl;
+  // int8_t imin = -128;    cout << int(imin) << endl;
 
-  // test Vector constructors
+
+  // Game constructors
+  //------------------
+
+  // ge::Game f(int8_t(4), string("yep"));
+  // ge::Game g(3,"word");
+
+  // // print results
+  // cout << f.name() << endl;
+  // cout << g.name() << endl;
+
+
+  // Vector constructors
+  //--------------------
+
   std::vector<ge::Game> vec;
   vec.reserve(2);
-  vec.emplace_back(int(0), string("asterisks"));
-  vec.emplace_back(1, "poker");
+  vec.emplace_back(uint(0), string("asterisks"));
+  vec.emplace_back(uint8_t(1), "poker");
 
-  // print results
   for( ge::Game i : vec ){
     cout << i.name() << endl;
   }
 
-  // test GameEngine 
+
+  // Go Again
+  //------------
+
+  ge::GameEngine games(vec);
+  games.setGoAgain2('y');  
+    // print results
+  cout << games.getGoAgain2() << endl;
+
+
+  // Game Engine
+  //------------
+
+  // manual construtors with normal array not fun
+  // ge::Game games[2] = { {4,"yep"},
+  //                       {3,"word"}
+  //                     };
+
+
   ge::GameEngine a(vec);
-  a.getGoAgain();
+  a.setGoAgain('y');
+  cout << a.getGoAgain() << endl;
+  a.start(a.getGameSelected());
 }
 
 

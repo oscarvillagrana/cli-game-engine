c2384574fb04cdd0b08c5332786db78f707f4995
Oscar Villagrana <Oscarvillagrana@github.com>
2021-05-01 20:15:52 -0700

    added cmake config

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@
 #include <vector>
 #include <stdint.h>
 #include <map>
+#include "utils/utils.cpp"
 using namespace std;
 
 
@@ -25,34 +26,34 @@ using namespace std;
 
 
 
-template <typename T> void disp_v(vector<T> & v);
-template <typename T> void msg( T & m );
-template <typename T> void msg( const char * m, const T & v );
+// template <typename T> void disp_v(vector<T> & v);
+// template <typename T> void msg( T & m );
+// template <typename T> void msg( const char * m, const T & v );
 
 
 
-template <typename T> 
-void msg( T & m ) { cout << m << endl; }
-template <typename T> 
-void msg( const char * m, const T & v ) { cout << m << ": " << v << endl; }
+// template <typename T> 
+// void msg( T & m ) { cout << m << endl; }
+// template <typename T> 
+// void msg( const char * m, const T & v ) { cout << m << ": " << v << endl; }
 
 
-// TODO: utils / ints used
-struct preciseInts
-{         
-   // signed integers precise bits
-   int8_t SmallInt = 0;                       //has max of 127
-   int16_t MediumInt;
-   int32_t LargeInt;
-   int64_t HugeInt;
+// // TODO: utils / ints used
+// struct preciseInts
+// {         
+//    // signed integers precise bits
+//    int8_t SmallInt = 0;                       //has max of 127
+//    int16_t MediumInt;
+//    int32_t LargeInt;
+//    int64_t HugeInt;
 
-   //unsigned precise integers
-   uint8_t SmallUInt = 0;                       //has max of 127
-   uint16_t MediumUInt;
-   uint32_t LargeUInt;
-   int64_t HugeUInt;
+//    //unsigned precise integers
+//    uint8_t SmallUInt = 0;                       //has max of 127
+//    uint16_t MediumUInt;
+//    uint32_t LargeUInt;
+//    int64_t HugeUInt;
 
-};
+// };
 
 
 
